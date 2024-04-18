8745513eb89d9fef7308cf3f85fe358ccd74fbb0
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-04-18 22:02:22 +0900

    feat : refreshtoken에대한 MongoDB 필드 추가

--- src/main/java/com/example/Backend/domain/user/User.java
+++ src/main/java/com/example/Backend/domain/user/User.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 //@NoArgsConstructor : 파라미터가 없는 디폴트 생성자를 생성
 //@AllArgsConstructor : 모든 필드 값을 파라미터로 받는 생성자를 생성
@@ -29,6 +31,18 @@ public class User extends BaseEntity {
         //super() : 조상의 생성자 호출하는 것.(생성자=(인스턴스 초기화 메서드), 초기화블록은 상속되지 않기 때문.)
         //@NoArgsConstructor는 조상 생성자는 호출 못하므로 사용 불가
     }
+
+    @Field("refresh_token")// MongoDB에서는 @Field를 사용하여 필드를 명시할 수 있습니다.
+    private String refreshToken;
+    //refreshtoken은 DB에저장, accesstoken은 ->stateless이므로 저장 X
+
+    public void updateRefreshToken(String refreshToken){
+        this.refreshToken=refreshToken;
+    }
+    public void destroyRefreshToken(){
+        this.refreshToken=null;
+    }
+
 
 }
 //MongoDB에서는 스키마가 고정되어 있지 않기 때문에, @Field 어노테이션과 같은 수단을 사용하여 필드 레벨에서 직접 not null 제약 조건을 적용할 수는 없습니다
--- src/main/java/com/example/Backend/global/SecurityConfig.java
+++ src/main/java/com/example/Backend/global/SecurityConfig.java
@@ -1,4 +1,10 @@
 package com.example.Backend.global;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class SecurityConfig {
+
+//    public void encodePassword(PasswordEncoder passwordEncoder){
+//        this.password=passwordEncoder.encode(password);
+//    }
 }
