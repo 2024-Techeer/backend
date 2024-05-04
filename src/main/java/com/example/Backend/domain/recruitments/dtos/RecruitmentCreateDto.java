dd3bf38cdac2da1e544948b1bbbba7e15127b68e
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-04 17:39:10 +0900

    feat: 모집글 생성 dto 구현

---
+++ src/main/java/com/example/Backend/domain/recruitments/dtos/RecruitmentCreateDto.java
@@ -0,0 +1,22 @@
+package com.example.Backend.domain.recruitments.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+public class RecruitmentCreateDto {
+    private String type;
+    private String title;
+    private int number;
+    private Date startDate;
+    private Date endDate;
+    private Date deadline;
+    private boolean closing;
+
+    private List<Long> positionIds;    // 포지션 ID 목록
+    private List<Long> techStackIds;  // 테크스택 ID 목록
+}
