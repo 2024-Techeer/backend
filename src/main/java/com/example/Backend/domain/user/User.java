00098ece7362abde55cb79bff1b0a2c498729535
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-04 17:41:58 +0900

    chore: entities 디렉토리 생성

--- src/main/java/com/example/Backend/domain/user/User.java
+++ src/main/java/com/example/Backend/domain/user/User.java
@@ -1,9 +1,8 @@
 package com.example.Backend.domain.user;
 
-import com.example.Backend.domain.common.BaseEntity;
+import com.example.Backend.domain.common.entities.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 //@NoArgsConstructor : 파라미터가 없는 디폴트 생성자를 생성
 //@AllArgsConstructor : 모든 필드 값을 파라미터로 받는 생성자를 생성
