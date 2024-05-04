25b1833e8a7bccc81d86cf75075507f5ce4234a1
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-04 18:23:06 +0900

    feat: 모집글 전체, 상세조회 api 생성

--- src/main/java/com/example/Backend/domain/recruitments/controllers/RecruitmentsController.java
+++ src/main/java/com/example/Backend/domain/recruitments/controllers/RecruitmentsController.java
@@ -1,15 +1,16 @@
 package com.example.Backend.domain.recruitments.controllers;
 
 import com.example.Backend.domain.recruitments.dtos.RecruitmentCreateDto;
+import com.example.Backend.domain.recruitments.dtos.RecruitmentDetailDto;
+import com.example.Backend.domain.recruitments.dtos.RecruitmentListDto;
 import com.example.Backend.domain.recruitments.entities.Recruitment;
 import com.example.Backend.domain.recruitments.services.RecruitmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -27,5 +28,16 @@ public class RecruitmentsController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "생성 실패"));
         }
+    }
+    @GetMapping
+    public ResponseEntity<List<RecruitmentListDto>> getAllRecruitments() {
+        List<RecruitmentListDto> recruitments = recruitmentService.getAllRecruitments();
+        return ResponseEntity.ok(recruitments);
+    }
+    @GetMapping("/{recruitmentId}")
+    public ResponseEntity<RecruitmentDetailDto> getRecruitmentById(@PathVariable Long recruitmentId) {
+        return recruitmentService.getRecruitmentById(recruitmentId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
