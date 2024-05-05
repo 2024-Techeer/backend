90ab694d1d2727032e9c74be32499870a4db2cc4
sunghyun <20190171@inu.ac.kr>
2024-05-05 21:01:27 +0900

    UserPosition (중간테이블) 생성

---
+++ src/main/java/com/example/Backend/domain/user/entities/UserPosition.java
@@ -0,0 +1,25 @@
+package com.example.Backend.domain.user.entities;
+
+import com.example.Backend.domain.common.entities.Position;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user_positions")
+public class UserPosition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id" , nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "position_id", nullable = false)
+    private Position position;
+}
