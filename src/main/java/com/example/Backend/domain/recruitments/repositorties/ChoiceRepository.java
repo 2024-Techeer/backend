4269c4ca900fa06444f1a54eeb6187f45832870d
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-13 00:28:24 +0900

    feat: findByAnswer 메소드 추가

--- src/main/java/com/example/Backend/domain/recruitments/repositorties/ChoiceRepository.java
+++ src/main/java/com/example/Backend/domain/recruitments/repositorties/ChoiceRepository.java
@@ -1,7 +1,11 @@
 package com.example.Backend.domain.recruitments.repositorties;
 
+import com.example.Backend.domain.recruitments.entities.Answer;
 import com.example.Backend.domain.recruitments.entities.Choice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChoiceRepository extends JpaRepository<Choice, Long> {
+    List<Choice> findByAnswer(Answer answer);
 }
