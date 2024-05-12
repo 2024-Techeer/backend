a81255f1569d497d827f1ba49b9955369c9646c9
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-13 00:28:37 +0900

    feat: findByApplication 메소드 추가

--- src/main/java/com/example/Backend/domain/recruitments/repositorties/SubmissionRepository.java
+++ src/main/java/com/example/Backend/domain/recruitments/repositorties/SubmissionRepository.java
@@ -3,5 +3,8 @@ package com.example.Backend.domain.recruitments.repositorties;
 import com.example.Backend.domain.recruitments.entities.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
+    List<Submission> findByApplicationId(Long applicationId);
 }
