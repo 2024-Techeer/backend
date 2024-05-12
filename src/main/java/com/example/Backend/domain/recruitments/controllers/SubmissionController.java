42c5991235a2f1966dacee3109b22bc34229c690
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-13 00:27:03 +0900

    feat: 제출서 조회 api 구현

--- src/main/java/com/example/Backend/domain/recruitments/controllers/SubmissionController.java
+++ src/main/java/com/example/Backend/domain/recruitments/controllers/SubmissionController.java
@@ -1,14 +1,13 @@
 package com.example.Backend.domain.recruitments.controllers;
 
+import com.example.Backend.domain.recruitments.dtos.SubmissionDetailDto;
+import com.example.Backend.domain.recruitments.dtos.SubmissionReadDto;
 import com.example.Backend.domain.recruitments.entities.Submission;
 import com.example.Backend.domain.recruitments.services.SubmissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -23,5 +22,22 @@ public class SubmissionController {
     public ResponseEntity<?> createSubmission(@PathVariable Long applicationId) {
         Submission submission = submissionService.createSubmission(applicationId);
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("id", submission.getId()));
+    }
+
+    // 제출서 전체 조회
+    @GetMapping
+    public ResponseEntity<SubmissionReadDto> getSubmissions(@PathVariable Long applicationId) {
+        SubmissionReadDto submissionDto = submissionService.getSubmissions(applicationId);
+        if (submissionDto == null || submissionDto.getSubmissionIds().isEmpty()) {
+            return ResponseEntity.notFound().build(); // 404 Not Found 응답
+        }
+        return ResponseEntity.ok(submissionDto); // 200 OK 응답과 함께 DTO 반환
+    }
+
+    @GetMapping("/{submissionId}")
+    public ResponseEntity<SubmissionDetailDto> getSubmission(@PathVariable Long submissionId) {
+        return submissionService.getSubmission(submissionId)
+                .map(submissionDetailDto -> ResponseEntity.ok(submissionDetailDto))  // 200 OK with data
+                .orElseGet(() -> ResponseEntity.notFound().build());  // 404 Not Found if empty
     }
 }
