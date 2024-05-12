4e037402b8a572251c17b188d83364622a0aaa9f
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-12 17:53:01 +0900

    chore: 신청서 api 엔드포인트 변경

--- src/main/java/com/example/Backend/domain/recruitments/controllers/ApplicationController.java
+++ src/main/java/com/example/Backend/domain/recruitments/controllers/ApplicationController.java
@@ -1,30 +1,30 @@
 package com.example.Backend.domain.recruitments.controllers;
 
+import com.example.Backend.domain.recruitments.dtos.ApplicationReadDto;
 import com.example.Backend.domain.recruitments.entities.Application;
 import com.example.Backend.domain.recruitments.services.ApplicationService;
 import com.example.Backend.domain.recruitments.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/applications")
+@RequestMapping("/api/v1/recruitments/{recruitmentId}/applications")
 public class ApplicationController {
 
     @Autowired
     private ApplicationService applicationService;
 
     // 신청서 생성
-    @PostMapping("/{recruitmentId}")
+    @PostMapping
     public ResponseEntity<?> createApplication(@PathVariable Long recruitmentId) {
         Application application = applicationService.createApplication(recruitmentId);
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("id", application.getId()));
     }
+
 
 }
