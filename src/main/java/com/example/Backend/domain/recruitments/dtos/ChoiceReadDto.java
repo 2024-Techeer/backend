a96c573372b238a6b82954c651dbdb2dc6aeeeaf
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-13 00:27:29 +0900

    feat: 선택 조회 dto 구현

---
+++ src/main/java/com/example/Backend/domain/recruitments/dtos/ChoiceReadDto.java
@@ -0,0 +1,10 @@
+package com.example.Backend.domain.recruitments.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChoiceReadDto {
+    private Long optionId;
+}
