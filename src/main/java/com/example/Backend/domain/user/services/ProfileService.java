e0f7ec4058220546c69a396f08b5baa78c5751fc
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-14 21:03:49 +0900

    refactor: 프로필 조회에 분야와 기술스택 조회도 적용

--- src/main/java/com/example/Backend/domain/user/services/ProfileService.java
+++ src/main/java/com/example/Backend/domain/user/services/ProfileService.java
@@ -21,6 +21,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProfileService {
@@ -95,6 +96,21 @@ public class ProfileService {
         profileViewDto.setResidence(user.getResidence());
         profileViewDto.setStatus(user.getStatus());
         profileViewDto.setGithub(user.getGithub());
+
+        // Position 이름 추출
+        List<String> positionNames = userPositionRepository.findByUser(user)
+                .stream()
+                .map(recruitmentPosition -> recruitmentPosition.getPosition().getName())
+                .collect(Collectors.toList());
+        profileViewDto.setPositions(positionNames);
+
+        // TechStack 이름 추출
+        List<String> techStackNames = userTechStackRepository.findByUser(user)
+                .stream()
+                .map(recruitmentTechStack -> recruitmentTechStack.getTechStack().getName())
+                .collect(Collectors.toList());
+        profileViewDto.setTechStacks(techStackNames);
+
         return profileViewDto;
     }
 
