9c36425006f502c6f81d8fe98c7bd8dd635e74c6
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-16 15:27:17 +0900

    chore: 질문 생성 API 엔드포인트 변경

--- src/main/java/com/example/Backend/domain/recruitments/controllers/applications/QuestionController.java
+++ src/main/java/com/example/Backend/domain/recruitments/controllers/applications/QuestionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/application/{applicationId}/questions")
+@RequestMapping("/api/v1/applications/{applicationId}/questions")
 public class QuestionController {
 
     @Autowired
