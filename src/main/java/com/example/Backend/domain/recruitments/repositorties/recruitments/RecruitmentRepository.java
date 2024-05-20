ffb91543a5c30bcd02f97be85efa0bc9d992536a
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-20 11:55:03 +0900

    feat: 내가 쓴 모집글 조회

--- src/main/java/com/example/Backend/domain/recruitments/repositorties/recruitments/RecruitmentRepository.java
+++ src/main/java/com/example/Backend/domain/recruitments/repositorties/recruitments/RecruitmentRepository.java
@@ -1,12 +1,13 @@
 package com.example.Backend.domain.recruitments.repositorties.recruitments;
 
 import com.example.Backend.domain.recruitments.entities.recruitments.Recruitment;
+import com.example.Backend.domain.user.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long>, JpaSpecificationExecutor<Recruitment>{
-
-
+    List<Recruitment> findByUser(User user);
 }
