59b0a95d563bac52197244428c90426356c96d0a
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-05-20 11:55:10 +0900

    feat: 내가 신청한 모집글 조회

--- src/main/java/com/example/Backend/domain/recruitments/repositorties/submissions/SubmissionRepository.java
+++ src/main/java/com/example/Backend/domain/recruitments/repositorties/submissions/SubmissionRepository.java
@@ -1,10 +1,12 @@
 package com.example.Backend.domain.recruitments.repositorties.submissions;
 
 import com.example.Backend.domain.recruitments.entities.submissions.Submission;
+import com.example.Backend.domain.user.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
     List<Submission> findByApplicationId(Long applicationId);
+    List<Submission> findByUser(User user);
 }
