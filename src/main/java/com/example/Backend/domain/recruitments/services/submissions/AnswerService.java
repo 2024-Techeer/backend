d6d13a38540ce9212729c68ec9c6410298f8b8fe
sunghyun <20190171@inu.ac.kr>
2024-06-03 16:11:57 +0900

    feat AnswerService에 file/descriptive/multiple 로직구현

--- src/main/java/com/example/Backend/domain/recruitments/services/submissions/AnswerService.java
+++ src/main/java/com/example/Backend/domain/recruitments/services/submissions/AnswerService.java
@@ -11,8 +11,13 @@ import com.example.Backend.domain.recruitments.repositorties.applications.Questi
 import com.example.Backend.domain.recruitments.repositorties.submissions.AnswerRepository;
 import com.example.Backend.domain.recruitments.repositorties.submissions.ChoiceRepository;
 import com.example.Backend.domain.recruitments.repositorties.submissions.SubmissionRepository;
+import com.example.Backend.global.s3.S3Exception;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class AnswerService {
@@ -26,8 +31,10 @@ public class AnswerService {
     private ChoiceRepository choiceRepository;
     @Autowired
     private OptionRepository optionRepository;
+    @Autowired
+    private S3FileService s3FileService;
 //submission : 제출서
-    public Answer createAnswer(AnswerCreateDto dto, Long questionId, Long submissionId) {
+    public Answer createAnswer(AnswerCreateDto dto, Long questionId, Long submissionId, MultipartFile file) {
         Question question = questionRepository.findById(questionId)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 질문입니다."));
 
@@ -37,24 +44,45 @@ public class AnswerService {
         Answer answer = new Answer();
         answer.setQuestion(question);
         answer.setSubmission(submission);
-        if(dto.getContent() != null) {
-            answer.setContent(dto.getContent());
+
+        if("descriptive".equals(question.getType())){
+            if(dto.getContent()!=null){
+                answer.setContent(dto.getContent());
+            }else{
+                throw new RuntimeException("서술형 답변 내용이 없습니다.");
+            }
+
+        }else if("file".equals(question.getType())){
+            if(file!=null){
+                try {
+                    String fileUrl= s3FileService.upload(file);
+                    answer.setContent(fileUrl);
+                }catch (S3Exception e){
+                    throw new RuntimeException("파일이 업로드 중 오류가 발생했습니다");
+                }
+            }else {
+                throw new RuntimeException("파일이 없습니다.");
+            }
+        } else if ("multiple".equals(question.getType())) {
+            answerRepository.save(answer);
+            List<Long> optionIds = dto.getOptionIds();
+            if(optionIds != null){
+                for (Long optionId : optionIds){
+                    Option option = optionRepository.findById(optionId)
+                            .orElseThrow(()-> new RuntimeException("존재하지 않은 선택지입니다."));
+                    Choice choice = new Choice();
+                    choice.setOption(option);
+                    choice.setAnswer(answer);
+                    choiceRepository.save(choice);
+                }
+            }
+            return answer;
+
+        }else {
+            throw new RuntimeException("알 수 없는 질문 타입입니다.");
         }
 
         answerRepository.save(answer);
-
-        if("multiple".equals(question.getType())) {
-             for(Long optionId : dto.getOptionIds()) {
-                 Option option = optionRepository.findById(optionId)
-                         .orElseThrow(() -> new RuntimeException("존재하지 않는 선택지입니다."));
-                 Choice choice = new Choice();
-                 choice.setOption(option);
-                 choice.setAnswer(answer);
-                 choiceRepository.save(choice);
-             }
-        }
-
         return answer;
-
     }
 }
